ab13518c23e17b41b3a09d93df321e2ce0e474ea
a-cs <acarneirosousa@gmail.com>
2022-04-04 22:24:55 -0300

    completing task

---
+++ js/activity.js
@@ -0,0 +1,5 @@
+const name = prompt("Qual o seu nome?", "Digite o seu nome.")
+const age = prompt("Qual a sua idade?", "Digite a sua idade.")
+const email = prompt("Qual o seu email?", "Digite o seu email.")
+
+alert(name + " tem " +age +" anos e seu email é "+ email)
